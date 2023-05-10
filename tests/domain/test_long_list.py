b6688c31be4e37ea610d7af73620876f029714cc
sepideh <s.sepideh.hoseinian@gmail.com>
2023-05-10 12:16:40 +0330

    Using Plan Asserts in pytest

---
+++ tests/domain/test_long_list.py
@@ -0,0 +1,4 @@
+def test_long_list():
+    result= ['this','is','a','long','list','with','tons','of','items']
+    expected=['this','is','the','long','list','with','tons','of','items']
+    assert result == expected
