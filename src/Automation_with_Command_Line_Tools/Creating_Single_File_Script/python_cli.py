72091d727130763d67f7b8f87fa596201072ed26
sepideh <s.sepideh.hoseinian@gmail.com>
2023-05-24 18:35:56 +0330

    using click

--- src/Automation_with_Command_Line_Tools/Creating_Single_File_Script/python_cli.py
+++ src/Automation_with_Command_Line_Tools/Creating_Single_File_Script/python_cli.py
@@ -1,23 +1,27 @@
 import sys
 import json
 import argparse
+import click
 def formater(string,sort_key=True,indent=4):
     loaded_json= json.loads(string)
     return json.dumps(loaded_json,sort_keys=sort_key,indent=indent)
-
-def main (path,no_sort):
-    if no_sort:
-        sort_key=True
-    else: sort_key=False
+@click.command()
+@click.argument('path',type=click.Path(exists=True))
+@click.option('--sort','-s', is_flag=True)
+def main (path,sort):
+    # if no_sort:
+    #     sort_key=True
+    # else: sort_key=False
 
     with open (path,'r') as _:
         print (
-            formater(_.read(),sort_key=True)
+            formater(_.read(),sort_key=sort)
               )
 if __name__=="__main__":
-    parser=argparse.ArgumentParser(description="this is a json formater method")
-    parser.add_argument("--sort",action="store_true",help="set sorting")
-    args=parser.parse_args()
-    print(args.sort)
-
+    # parser=argparse.ArgumentParser(description="this is a json formater method")
+    # parser.add_argument("--sort",action="store_true",help="set sorting")
+    # args=parser.parse_args()
+    # print(args.sort)
     #main(sys.argv[-1],no_sort=False)
+    # main(sys.argv[-1])
+    main()
